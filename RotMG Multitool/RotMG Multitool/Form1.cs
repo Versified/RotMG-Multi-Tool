9ba2352f054993ec84b5d49cec594689a962ff18
BlackRayquaza <shadowrealmrotmg@gmail.com>
2015-12-28 16:43:34 +0100

    Metrooooooooooo

--- RotMG Multitool/RotMG Multitool/Form1.cs
+++ RotMG Multitool/RotMG Multitool/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MetroFramework.Forms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,7 +11,7 @@ using System.Windows.Forms;
 
 namespace RotMG_Multitool
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MetroForm
     {
         public Form1()
         {
